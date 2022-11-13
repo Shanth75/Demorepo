97310e78105b8654dbeed978d71224d0bf8ac53e
shanth75 <shanker07@hotmail.com>
2022-11-12 19:38:48 -0600

    2nd commit

--- src/Sum.java
+++ src/Sum.java
@@ -1,8 +1,20 @@
 public class Sum {
     public static void main(String[] args) {
+        multiply times= new multiply();
+        times.a=10;
+        times.b=20;
+        times.result();
         int a=25;
         int b=35;
         int sum=a+b;
         System.out.println(sum);
+    }
+}
+class multiply{
+    int a;
+    int b;
+
+    void result(){
+        System.out.println(a*b);
     }
 }
